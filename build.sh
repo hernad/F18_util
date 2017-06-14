3844ad357ec843bf1e92aed59454db7dbdac7aff
hernad <hernad@bring.out.ba>
2017-06-14 04:27:53 +0200

    build sve platforme

--- build.sh
+++ build.sh
@@ -2,4 +2,25 @@
 
 PROG=F18_sha256sum
 
-GOOS=windows GOARCH=386 go build -o $PROG.exe $PROG.go
+export GOOS=windows
+export GOARCH=386 
+go build -o $PROG.exe $PROG.go
+CMD="zip ${PROG}_${GOOS}_${GOARCH}.zip $PROG.exe"
+echo $CMD
+eval $CMD
+
+export GOOS=darwin 
+export GOARCH=amd64
+
+go build -o $PROG $PROG.go
+CMD="zip ${PROG}_${GOOS}_${GOARCH}.zip $PROG"
+echo $CMD
+eval $CMD
+
+export GOOS=linux
+export GOARCH=386 
+go build -o $PROG $PROG.go
+CMD="zip ${PROG}_${GOOS}_${GOARCH}.zip $PROG"
+echo $CMD
+eval $CMD
+
