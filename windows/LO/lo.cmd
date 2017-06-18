16bc5ae6de30adc9a0465a6e2b3c824595b3faa9
hernad <hernad@bring.out.ba>
2017-06-18 15:33:24 +0200

    lo.cmd

---
+++ windows/LO/lo.cmd
@@ -0,0 +1,39 @@
+REM https://stackoverflow.com/questions/1645843/resolve-absolute-path-from-relative-path-and-or-file-name
+REM %~dp0 is "C:\temp\"
+set PATH0=%PATH%
+set THISDIR=%~dp0
+set PATH=%THISDIR%\program;c:\windows\system32
+
+
+set FN=%1
+
+if "%2"==""  goto :full
+set FN=%FN% %2
+
+if "%3"==""  goto :full
+set FN=%FN% %3
+
+
+if "%4"==""  goto :full
+set FN=%FN% %4
+
+if "%5"==""  goto :full
+set FN=%FN% %5
+
+if "%6"==""  goto :full
+set FN=%FN% %6
+
+:full
+
+
+if not exist "%APPDATA%\LO\4" (
+  mkdir "%APPDATA%\LO"
+  mkdir "%APPDATA%\LO\4"
+  xcopy /s "%THISDIR%\APPDATA\*.*" "%APPDATA%\LO\4\"
+)
+
+start /MAX %THISDIR%\program\soffice.exe "%FN%"
+
+REM start focus.vbs "%FN%.conv.txt"
+
+set PATH=%PATH0%
