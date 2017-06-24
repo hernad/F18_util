8ed1b64ce91919fb469c9bea634cfa7509f4a481
hernad <hernad@bring.out.ba>
2017-06-24 20:09:22 +0200

    update lo.cmd

--- windows/LO/lo.cmd
+++ windows/LO/lo.cmd
@@ -1,11 +1,9 @@
 REM 007 VERZIJA
-
 REM https://stackoverflow.com/questions/1645843/resolve-absolute-path-from-relative-path-and-or-file-name
 REM %~dp0 is "C:\temp\"
 set PATH0=%PATH%
 set THISDIR=%~dp0
 set PATH=%THISDIR%\program;c:\windows\system32
-
 set FILE=%1
 
 if not exist "%APPDATA%\LO\4" (
